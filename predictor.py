890ba08e230c0df4549a6b01df2c7ea12c6a1c5a
hammadulmustafa-albarvi <hammadulmustafa2007@gmail.com>
2026-03-07 13:06:56 +0500

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-from sklearn.metrics import r2_score,mean_absolute_error
+# from sklearn.metrics import r2_score,mean_absolute_error
 
 import pickle
 
